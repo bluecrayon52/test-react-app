882c573c40e5d19db9666d11a9433304eacefb49
bluecrayon52 <16687465+bluecrayon52@users.noreply.github.com>
2019-09-05 11:50:54 -0700

    added logout button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,44 +11,62 @@ Amplify.configure({
     region: 'us-east-1',
     // OPTIONAL - Amazon Cognito User Pool ID
     userPoolId: process.env.REACT_APP_USER_POOL_ID,
+    // OPTIONAL - Amazon Cognito Web Client ID
     userPoolWebClientId: process.env.REACT_APP_USER_POOL_WEB_CLIENT_ID,
+    // OPTIONAL - Configuration for cookie storage
+    // Note: if the secure flag is set to true, then the cookie transmission requires a secure protocol
     cookieStorage: {
+      // OPTIONAL - Cookie path
       path: '/',
+      // OPTIONAL - Cookie expiration in days
       expires: '',
+      // REQUIRED - Cookie domain (only required if cookieStorage is provided)
       domain: window.location.hostname,
+      // OPTIONAL - Cookie secure flag
+      // Either true or false, indicating if the cookie transmission requires a secure protocol (https).
       secure: true,
     },
+    // your Cognito Hosted UI configuration
     oauth: {
+      // Domain name
       domain: process.env.REACT_APP_USER_POOL_AUTH_DOMAIN,
+      // Authorized scopes
       scope: process.env.REACT_APP_USER_POOL_SCOPES.split(','),
+      // Callback URL
       redirectSignIn: `https://${window.location.hostname}${process.env.REACT_APP_USER_POOL_REDIRECT_PATH_SIGN_IN}`,
+      // Sign out URL
       redirectSignOut: `https://${window.location.hostname}${process.env.REACT_APP_USER_POOL_REDIRECT_PATH_SIGN_OUT}`,
-      responseType: 'code'
+      // 'code' for Authorization code grant, 
+      // 'token' for Implicit grant
+      // Note that REFRESH token will only be generated when the responseType is code
+      responseType: 'code' 
     }
   }
 });
 
 const decodeToken = (token) => {
-  const tokenBody = token.split('.')[1];
-  const decodableTokenBody = tokenBody.replace(/-/g, '+').replace(/_/g, '/');
-  return JSON.parse(window.atob(decodableTokenBody));
+  // get payload portion [header . payload . signature]
+  const base64Url = token.split('.')[1];
+  // from base64url encoding to base64 
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  // decode base64 encoded string
+  return JSON.parse(window.atob(base64));
 }
 
 const App = () => {
+
   const [state, setState] = useState({
-    email: undefined,
-    username: undefined,
+    email: undefined
   });
 
   useEffect(() => {
+    // returns a CognitoUserSession object which contains JWT accessToken, idToken, and refreshToken.
+    // will automatically refresh the accessToken and idToken if tokens are expired and a valid refreshToken presented
+    Auth.currentSession().then(
+      userSession => setState({ email: decodeToken(userSession.getIdToken().getJwtToken()).email })
+    );
 
-    Auth.currentSession()
-      .then(user => setState({
-        username: user.username,
-        email: decodeToken(user.getIdToken().getJwtToken()).email,
-      }));
-
-    // Schedule check and refresh (when needed) of JWT's every 5 min:
+    // Schedule check and refresh (when needed) of JWT's every 5 min: (5 * 60 * 1000 = 300,000 milliseconds)
     const i = setInterval(() => Auth.currentSession(), 5 * 60 * 1000);
     return () => clearInterval(i);
   }, []);
@@ -56,18 +74,10 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <h2> Welcome To The Private Octank Page</h2>
+        <p>Your email: <strong>{state.email}</strong></p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi Andy! Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className="SO-button" onClick={() => Auth.signOut()}>Sign out</button>
       </header>
     </div>
   );
